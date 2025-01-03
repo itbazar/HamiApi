633980aff474f0d39cca756f1477e52ed31b9714
RKH <raziehkhanzadeh77@gmail.com>
2025-01-03 22:17:09 +0330

    Implement Patient Events

---
+++ Domain/Models/Hami/Events/PatientApprovedDomainEvent.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Primitives;
+
+namespace Domain.Models.Hami.Events;
+
+public record PatientApprovedDomainEvent(
+    Guid userGroupId,
+    string UserId,
+    string PhoneNumber) : DomainEvent(userGroupId);
+
---
+++ Domain/Models/Hami/Events/PatientCreatedDomainEvent.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Primitives;
+
+namespace Domain.Models.Hami.Events;
+
+public record PatientCreatedDomainEvent(
+    Guid userMedicalId,
+    string UserId,
+    string PhoneNumber) : DomainEvent(userMedicalId);
+
